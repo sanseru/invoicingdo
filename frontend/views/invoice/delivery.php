bd36b912578d9257dd82fe73498d706ae56764b3
sanseru <sanseru1@yahoo.com>
2021-05-21 15:50:44 +0700

    Perbaikan

--- frontend/views/invoice/delivery.php
+++ frontend/views/invoice/delivery.php
@@ -116,10 +116,12 @@ use yii\helpers\Html;
         <th width="60%" align="center">Name</th>
         <th width="40%" align="center">Nett Weight</th>
     </tr>
+    <?php 
+    ?>
     <?php foreach($leadsCount as $m):?>
     <tr>
-        <td><?= $m->item;?></td>
-        <td align="center"><?= $m->cnt;?></td>
+        <td><?= $m['item'];?></td>
+        <td align="center"><?= $m['cnt'];?></td>
 
     </tr>
 
